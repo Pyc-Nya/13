6da0ff9d4d10dc837c0d15a33883e738043e745e
Pyc-Nya <premiyaoskara@gmail.com>
2023-09-03 18:46:24 +0300

    mobile version and endgame screen

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 let table = document.querySelector('.table');
 let refreshButton = document.querySelector('.text__refresh span');
 let moveInfo = document.querySelector('.text__item');
+let endGameScreen = document.querySelector('.endgame');
+let endGameRefreshButton = document.querySelector('.endgame__button');
+let endGameText = document.querySelector('.endgame__text');
+
 
 let map = [
     [" ", " ", " "],
@@ -12,6 +16,8 @@ let map = [
 
 table.addEventListener("click", handler);
 refreshButton.addEventListener("click", refresh);
+endGameRefreshButton.addEventListener("click", newGame);
+let computerMove = true;
 
 function handler(e) {
     if (e.target.tagName != 'TD') return;
@@ -22,21 +28,23 @@ function handler(e) {
 
     let target = e.target.querySelector('.cross');
 
+    if (computerMove) {
+        target.style.display = "inline-block";
+        map[rowIndex][cellIndex] = "x";
+        computerMove = false;
+        if (game()) return;
 
-    target.style.display = "inline-block";
-    map[rowIndex][cellIndex] = "x";
-    if (game()) return;
+        moveInfo.innerHTML = 'Ход Компьютера';
 
-    moveInfo.innerHTML = 'Ход Компьютера';
-
-    setTimeout(() => {
-        makeMoveComputer(map);
-        moveInfo.innerHTML = 'Ваш ход';
         setTimeout(() => {
-            if (game()) return;
-        }, 5);
-    }, 500);
-    
+            makeMoveComputer(map);
+            moveInfo.innerHTML = 'Ваш ход';
+            computerMove = true;
+            setTimeout(() => {
+                if (game()) return;
+            }, 50);
+        }, 500);
+    }
 }
 
 
@@ -52,15 +60,31 @@ function refresh() {
         [" ", " ", " "],
         [" ", " ", " "]
     ];
+    computerMove = true;
 }
 
 function game() {
     let gameStatus = checkEndGame(map);
     if (gameStatus[0]) {
         setTimeout(() => {
-            alert(gameStatus[1]);
-            refresh();
-        }, 5);
+            endGameText.innerHTML = gameStatus[1];
+
+            endGameScreen.style.display = 'flex';
+            setTimeout(() => {
+                endGameScreen.style.opacity = 1;
+            }, 30);
+        }, 50);
     }
     return gameStatus[0];
 }
+
+function newGame() {
+
+    endGameScreen.style.opacity = 0;
+    setTimeout(() => {
+        endGameScreen.style.display = 'none';
+    }, 500);
+    
+    refresh();
+}
+
